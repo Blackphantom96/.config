7e0779bf5e513693039831d6ec48ccb3804db36d
Juan Andres Moreno <juan.andres.208@hotmail.com>
2018-05-12 19:26:34 -0500

    Se agrego la capa de perzonalizacion

--- Install Apps.sh
+++ Install Apps.sh
@@ -42,6 +42,28 @@ echo -e " ${GREEN} instalando kdeConnect${NC}";
 sudo apt install -y kdeconnect-plasma;
 echo -e " ${GREEN} instalando gnome tweaks${NC}";
 sudo apt install -y gnome-tweak-tool;
+echo -e " ${GREEN} instalando Okular${NC}";
+sudo apt install -y okular;
+echo -e " ${GREEN} instalando GNome Shell Extension${NC}";
+sudo apt-get install -y chrome-gnome-shell;
+echo -e " ${GREEN} instalando Pomodoro para Gnome Shell${NC}";
+sudo apt-get install -y gnome-shell-pomodoro;
+echo -e " ${GREEN} instalando Source Code Pro${NC}";
+wget https://github.com/adobe-fonts/source-code-pro/archive/2.030R-ro/1.050R-it.zip;
+unzip 1.050R-it.zip;
+mkdir -p ~/.fonts;
+cp source-code-pro-*-it/OTF/*.otf ~/.fonts/;
+echo -e " ${GREEN} instalando Personalizacion${NC}";
+cd /tmp && rm -rf flat-remix &&
+git clone https://github.com/daniruiz/flat-remix &&
+mkdir -p ~/.icons && cp -r flat-remix/Flat-Remix* ~/.icons/ &&
+gsettings set org.gnome.desktop.interface icon-theme "Flat-Remix";
+sudo add-apt-repository ppa:ravefinity-project/ppa;
+sudo apt-get update;
+sudo apt-get install -y vivacious-colors ;
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' >> /etc/apt/sources.list.d/arc-theme.list";  
+sudo apt update  ;
+sudo apt install arc-theme ; 
 echo -e " ${GREEN} instalando netbeans${NC}";
 sudo chmod +x netbeans-8.2-linux.sh;
 ./netbeans-8.2-linux.sh;
